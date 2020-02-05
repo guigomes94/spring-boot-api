5a7c82c824e0b23d185350e5837358262ad1cb94
guigomes94 <guigomes94@outlook.com>
2020-02-04 23:50:33 -0300

    bean

--- src/main/java/com/surittec/api/repositories/UsuarioRepository.java
+++ src/main/java/com/surittec/api/repositories/UsuarioRepository.java
@@ -3,11 +3,9 @@ package com.surittec.api.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.surittec.api.models.Usuario;
 
-@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	
 	public Optional<Usuario> findByNome(String nome);
